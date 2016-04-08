4784c761fce6d598a9be29c777594d53b37d6698
george santana <george_santana@outlook.com>
2016-04-07 22:19:54 -0300

    Aula 6 - Entity

---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfdbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using Quiron.LojaVirtual.Dominio.Entidade;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfdbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+
+}
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/ProdutosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quiron.LojaVirtual.Dominio.Entidade;
+
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfdbContext _context = new EfdbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+
+    }
+}
